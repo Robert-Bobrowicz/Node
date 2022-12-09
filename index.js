fe1808ca589ad6050029dd9bf1cfcd916928591e
unknown <r.bobrowicz@live.com>
2022-12-09 11:02:01 +0100

    Node.js

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
     res.send('Hello express server!')
 })
 
+app.get('/companies/:name', (req, res) => {
+    console.log('Someone is looking for a company:', req.params);
+    const { name } = req.params;
+    const companies = [
+        { slug: "createserver", name: "CreateServer.com" },
+        { slug: "rbdevelopment", name: "RB_Development.Inc" }
+    ];
+
+    const company = companies.find(x => x.slug === name);
+
+    res.send(`Company name: ${company.name}`)
+})
+
 app.get('/kontakt', (req, res) => {
     res.send('Dane kontaktowe')
 })
