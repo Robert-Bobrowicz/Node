09ac9c5bbc71b8bf6c078d69165805abfce45e3c
unknown <r.bobrowicz@live.com>
2023-02-26 13:15:36 +0100

    mongodb explained

---
+++ db-mongodb.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+const dbName = 'node';
+
+
+async function main() {
+    await client.connect();
+    console.log('successfully connected to DB');
+
+    //create new collection 
+    const db = client.db(dbName);
+    await db
+        .collection('companies')
+        .insertOne({ slug: "skyshow", name: "SkyShow International Entertainment Ltd." });
+
+    //find element(s) in DB
+    const res = await db.collection('companies').findOne({ slug: "skyshow" });
+    const resAll = await db.collection('companies').find({}).toArray();
+    console.log(res);
+    console.log(resAll);
+
+    //delete element(s) from DB collection
+    await db.collection('companies').deleteMany({ slug: "skyshow" });
+
+    // close connection to DB 
+    // client.close(); //lub dodać finnally() do main()
+}
+
+main()
+    .catch(ex => console.log('Something went wrong. ', ex))
+    .finally(() => client.close());
