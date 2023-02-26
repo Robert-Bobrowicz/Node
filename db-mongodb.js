2572f4a9e2b3a38500588d7adbf6b17f1b6a045c
unknown <r.bobrowicz@live.com>
2023-02-26 13:47:07 +0100

    ObjectId explained

--- db-mongodb.js
+++ db-mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const client = new MongoClient('mongodb://127.0.0.1:27017');
 const dbName = 'node';
@@ -17,11 +17,13 @@ async function main() {
     //find element(s) in DB
     const res = await db.collection('companies').findOne({ slug: "skyshow" });
     const resAll = await db.collection('companies').find({}).toArray();
+    const resId = await db.collection('companies').find({ _id: new ObjectId('63fb5254213ae46678a53281') }).toArray();
     console.log(res);
     console.log(resAll);
+    console.log(resId);
 
     //delete element(s) from DB collection
-    await db.collection('companies').deleteMany({ slug: "skyshow" });
+    // await db.collection('companies').deleteMany({ slug: "skyshow" });
 
     // close connection to DB 
     // client.close(); //lub dodać finnally() do main()
