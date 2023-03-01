ba2e4c91c773c6d5d1e5395cc6f28a8bc3651caa
unknown <r.bobrowicz@live.com>
2023-03-01 14:01:18 +0100

    stop create new elements in DB

--- app/db/db-mongoose.js
+++ app/db/db-mongoose.js
@@ -3,29 +3,29 @@ const Company = require('./models/company');
 const { dbUrl } = require('../config');
 
 mongoose.set('strictQuery', true);
-mongoose.connect(dbUrl); //po slashu podaję nazwę bazy do której się łaczę
+mongoose.connect(dbUrl, () => console.log('db connected')); //po slashu podaję nazwę bazy do której się łaczę
 
 //create element(s) in DB
-async function createEl() {
-    const company = new Company({
-        slug: '   skyStar-2   ', //'slug', //'skylink-1',
-        name: 'SkyLink-Star 2 Inc.'
-    });
+// async function createEl() {
+//     const company = new Company({
+//         slug: '   skyStar-2   ', //'slug', //'skylink-1',
+//         name: 'SkyLink-Star 2 Inc.'
+//     });
 
-    try {
-        await company.save();
-    } catch (err) {
-        console.log('Smth went wrong to save element(s) to DB');
-        for (const key in err.errors) {
-            console.log(err.errors[key].message);
-        }
-    }
-}
+//     try {
+//         await company.save();
+//     } catch (err) {
+//         console.log('Smth went wrong to save element(s) to DB');
+//         for (const key in err.errors) {
+//             console.log(err.errors[key].message);
+//         }
+//     }
+// }
 
-createEl();
+// createEl();
 
 //find element(s) in DB
-Company.find({}, (err, docs) => {
-    if (err) console.log('Smth went wrong');
-    console.log(docs);
-});
+// Company.find({}, (err, docs) => {
+//     if (err) console.log('Smth went wrong');
+//     console.log(docs);
+// });
