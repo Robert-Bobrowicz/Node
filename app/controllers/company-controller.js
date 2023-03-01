27907c9525a710a5b1e1ce8d7d69ce1b5309e72e
unknown <r.bobrowicz@live.com>
2023-03-01 09:16:15 +0100

    views: all companies displayed

--- app/controllers/company-controller.js
+++ app/controllers/company-controller.js
@@ -1,6 +1,20 @@
+const Company = require('../db/models/company');
 const chalk = require('chalk');
 
 class CompanyController {
+
+    async showAllCompanies(req, res) {
+        const companies = await Company.find({});
+
+        console.log(chalk.greenBright(`All ${companies.length} companies in DB displayed.`));
+        // console.log(companies);
+        res.render('companies', {
+            companies,
+            url: req.url,
+            title: 'All Companies'
+        });
+    };
+
     showCompany(req, res) {
         console.log(chalk.red('Someone is looking for a company:', req.url));
         const { name } = req.params;
--- app/routes/web.js
+++ app/routes/web.js
@@ -7,6 +7,7 @@ const ContactControler = require('../controllers/contact-contoller');
 
 
 router.get('/', PageController.showHome);
+router.get('/companies', CompanyController.showAllCompanies);
 router.get('/companies/:name', CompanyController.showCompany);
 router.get('/kontakt', ContactControler.showContact);
 router.get('*', PageController.showNotFound);
