5b802994ef7d24f07e5dfeafc7990000badb40b6
unknown <r.bobrowicz@live.com>
2023-04-10 20:58:18 +0200

    api token

--- app/middlewares/is-auth-api-middleware.js
+++ app/middlewares/is-auth-api-middleware.js
@@ -5,11 +5,11 @@ module.exports = async function (req, res, next) {
     const user = await User.findOne({ apiToken: token });
 
     if (!token) {
-        res.status(403).json({ message: '403 Access forbidden' });
+        res.status(403).json({ message: '#403: Access forbidden' });
     }
 
     if (!user) {
-        res.status(403).json({ message: '403 Access forbidden' });
+        res.status(403).json({ message: '#403: Access forbidden' });
     }
 
     req.user = user;
